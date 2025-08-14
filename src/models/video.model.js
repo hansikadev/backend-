7355817c8d4ff08c587a00fa6f82e91e5c7d6f6e
delta <tanwarhansika123@gmail.com>
2025-08-15 01:33:30 +0530

    add user and vid model

---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggeratePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema= new Schema(
+    {
+        videofile:{
+            type:String, //cloudinary url
+            required:true
+        },
+        thumbnail:{
+            type:String, //cloudinary url
+            required:true,
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"user"
+        },
+        title:{
+            type:String,
+            required:true
+        },
+        description:{
+            type:String,
+            required:true
+        },
+        duration:{
+            type:Number,
+            required:true
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type:Boolean,
+            default:true //This field indicates whether the video is published or not. If true, the video is publicly accessible; if false, it might be private or unlisted.
+        }
+    },
+    {timestamps:true}
+)
+
+videoSchema.plugin(mongooseAggeratePaginate);
+export const video=mongoose.model("video",videoSchema);
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -23,3 +23,8 @@ class ApiError extends Error{
 }
 
 export {ApiError}
+
+// Normally, when something goes wrong in JavaScript, you use the built-in Error class to create an error.
+// This ApiError extends (class ApiError extends Error) that built-in Error so you can:
+// 1) Add extra information about the error (like statusCode, errors[]).
+// 2) Use it specifically for API responses, so you can send a nice JSON error to the client instead of a messy default.
