51eabed4b23b3dfb8af23d67e141ad0432457f01
delta <tanwarhansika123@gmail.com>
2025-08-15 19:15:34 +0530

    all setup configured !

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+import {asyncHandler} from "../utils/asyncHandler.js";
+
+const registeruser= asyncHandler(async(req,res)=>{
+    res.status(200).json({
+        message:"okayy",
+    })
+})
+
+export {registeruser};
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,14 +2,16 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './pulic/temp')
+    cb(null, './public/temp')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.originalname)
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
   }
 })
 
 export const upload = multer({
     storage: storage, 
 })
+
+
